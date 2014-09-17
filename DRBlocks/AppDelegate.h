6f522d208eede1c5de5d9078d8851d0d0a2f6015
Danny Ricciotti <dan.ricciotti@gmail.com>
2014-09-17 13:35:46 -0400

    Initial Commit

---
+++ DRBlocks/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DRBlocks
+//
+//  Created by Danny Ricciotti on 9/17/14.
+//  Copyright (c) 2014 Danny Ricciotti. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DRBlocks/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DRBlocks
+//
+//  Created by Danny Ricciotti on 9/17/14.
+//  Copyright (c) 2014 Danny Ricciotti. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
